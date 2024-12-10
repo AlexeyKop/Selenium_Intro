89b9b76bdb79e3cade4f5b2617f8fc4e0e7ec1b3
AlexeyKop <a.kop@bk.ru>
2024-12-10 19:10:37 +0400

    Урок 3. Лекция: Паттерны проектирования

--- CourseMaterials_Lecture1_Result_Two_tests/src/test/java/org/example/GeekBrainsStandTests.java
+++ CourseMaterials_Lecture1_Result_Two_tests/src/test/java/org/example/GeekBrainsStandTests.java
@@ -3,7 +3,6 @@ package org.example;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
